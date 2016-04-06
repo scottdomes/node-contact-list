1c089f25acdab9bdf5c982ca12a853213df5c54b
scottdomes <scottdomes@gmail.com>
2016-04-06 11:38:04 -0700

    First commit! Set up express and mongodb

---
+++ routes/index.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* GET Hello World page. */
+router.get('/helloworld', function(req, res) {
+  res.render('helloworld', { title: 'Hello, world!!' });
+});
+
+/* GET Contactlist page */
+router.get('/contactlist', function(req, res) {
+  var db = req.db;
+  var collection = db.get('contactcollection');
+  collection.find({}, {}, function(e, docs) {
+    res.render('contactlist', {
+      "contactlist" : docs
+    });
+  });
+});
+
+module.exports = router;
