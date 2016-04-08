551ded1ae72bd843b6532d5c242e28a644879719
scottdomes <scottdomes@gmail.com>
2016-04-08 12:46:16 -0700

    Fixed typo in config

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
         db: 'mongodb://localhost:27017/nodecontacts',
 
     },
-    product: {
+    production: {
         db: 'mongodb://heroku_rnqjrtqt:la8iakh0sdsc2nhp7qmr264qp0@ds021000.mlab.com:21000/heroku_rnqjrtqt',
     }
 };
